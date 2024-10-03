bb208ff04042d3e6a36a812d9ee46520cb30d01c
Audmiral <joshua.gulbin@gmail.com>
2024-10-03 11:18:44 +0800

    fix

--- task2.py
+++ task2.py
@@ -1,6 +1,7 @@
-list_of_fruits = ["banana", "apple", "orange," "mango"]
+list_of_fruits = ["banana", "apple", "orange", "mango"]
 
 for f in list_of_fruits:
+    
     if f == "banana":
         print("hellow banana")
     elif f == "apple":
