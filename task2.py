d174640134f30462d59378c8e91da93d1a3a3538
Audmiral <joshua.gulbin@gmail.com>
2024-10-03 11:09:57 +0800

    fruitArray

---
+++ task2.py
@@ -0,0 +1,9 @@
+list_of_fruits = ["banana", "apple", "orange," "mango"]
+
+for f in list_of_fruits:
+    if f == "banana":
+        print("hellow banana")
+    elif f == "apple":
+        print("hellow apple")
+    else:
+        print("no")
