d9713b69879c7705a30ef3ff70129bf094873b87
Audmiral <joshua.gulbin@gmail.com>
2024-10-03 10:59:50 +0800

    fave fruits

---
+++ task1.py
@@ -0,0 +1,8 @@
+num = int(input("How many fruits do you like? "))
+faveFruits = []
+
+for n in range(num):
+    fruit = input("What fruits do you like? ")
+    faveFruits.append(fruit)
+
+print(faveFruits)
